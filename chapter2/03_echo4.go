55a5b713e15b5a33fdeab70beb43b82e7b2dbea4
Neto Godoi <netogodoi.dev@gmail.com>
2021-01-20 19:30:38 -0300

    Simple CLI to receive args in flags

---
+++ chapter2/03_echo4.go
@@ -0,0 +1,19 @@
+//Show all args from command line
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
+
+var n = flag.Bool("n", false, "omit trailing newline")
+var sep = flag.String("s", " ", "separator")
+
+func main() {
+	flag.Parse()
+	fmt.Print(strings.Join(flag.Args(), *sep))
+	if !*n {
+		fmt.Println()
+	}
+}
