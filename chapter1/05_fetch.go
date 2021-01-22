da127353188497edb78aee46d85fc72288c93a63
Neto Godoi <netogodoi.dev@gmail.com>
2021-01-21 21:24:24 -0300

    Little changes in the way that read and print request result

--- chapter1/05_fetch.go
+++ chapter1/05_fetch.go
@@ -3,24 +3,33 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
-		resp, err := http.Get(url)
+		normalizedUrl, prefix := url, "http://"
+		if !strings.HasPrefix(url, prefix) {
+			normalizedUrl = prefix + url
+		}
+		resp, err := http.Get(normalizedUrl)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
-		b, err := ioutil.ReadAll(resp.Body)
+		status := resp.Status
+		fmt.Printf("Status code %s\n", status)
+		// b, err := ioutil.ReadAll(resp.Body)
+		_, err = io.Copy(os.Stdout, resp.Body)
 		resp.Body.Close()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch - reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
-		fmt.Printf("%s\n", b)
+
+		// fmt.Printf("%s\n", b)
 	}
 }
